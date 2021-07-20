dcbe84cd5f64a999cc90d14bcd5354042e43d60c
Bibindasb <75476488+bibindasb@users.noreply.github.com>
2021-07-20 08:33:16 -0700

    phoenix: Parts: Nuked parts

--- device.mk
+++ device.mk
@@ -183,11 +183,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 
 # Device Settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
+#PRODUCT_PACKAGES += \
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-parts.xml
 
 # Display/Graphics
 PRODUCT_PACKAGES += \
