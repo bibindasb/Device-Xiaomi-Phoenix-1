140d294998850932d899b91856494c30749a6e35
Bibindasb <75476488+bibindasb@users.noreply.github.com>
2021-07-24 21:36:50 +0530

    phoenix: SP update

--- ancient_phoenix.mk
+++ ancient_phoenix.mk
@@ -26,6 +26,6 @@ ANCIENT_GAPPS=true
 # Maintainer Properties
 ANCIENT_OFFICIAL := true
 
-BUILD_FINGERPRINT := POCO/phoenixin/phoenixin:11/RKQ1.200826.002/V12.1.3.0.RGHINXM:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:12/SPB3.210618.013/7533405:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
